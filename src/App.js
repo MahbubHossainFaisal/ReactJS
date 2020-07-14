807a0bc275554053ddd6cd5483aa4b86a5c2a7be
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-07-15 02:25:58 +0600

    React Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React,{ Component } from "react";
 import logo from "./logo.svg";
 import {Navbar, NavbarBrand} from "reactstrap";
 //importing MenuComponent
 import Menu from "./components/MenuComponent";
 import "./App.css";
+import { DISHES } from "./shared/dishes"
 
-function App() {
-    return ( <
-        div>
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            dishes:DISHES
+        };
+    }
+
+    render(){
+        return (<
+            div>
             <Navbar dark color="primary">
-            <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
+                <div className="container">
+                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                </div>
             </Navbar>
 
-            <Menu />
-         </div>
-    );
+            <Menu dishes={this.state.dishes} />
+        </div>
+        );
+    }
 }
 
 export default App;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,7 +1,7 @@
 //This import will allow to create a react component
 import React, { Component } from "react";
 //in order to construct menu
-import { Media } from "reactstrap";
+import { Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle } from "reactstrap";
 
 
 //Adding a menu component 
@@ -10,52 +10,35 @@ class Menu extends Component{
         super(props); //This is required whenever to define e class component
 
         this.state ={
-            //this stores the properties related to this component that we can make use of
-            dishes: [
-                //javascript object that contains list of dishes
-                {
-                    id: 0,
-                    name: 'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '4.99',
-                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-                },
-                {
-                    id: 1,
-                    name: 'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label: '',
-                    price: '1.99',
-                    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-                },
-                {
-                    id: 2,
-                    name: 'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label: 'New',
-                    price: '1.99',
-                    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-                },
-                {
-                    id: 3,
-                    name: 'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label: '',
-                    price: '2.99',
-                    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-                }
-            ],
+           selectedDish: null
+        }
+       
+    }
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish });
+    }
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle> {dish.name} </CardTitle>
+                        <CardText> {dish.description} </CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
         }
     }
 
     render(){ 
         //This method will return the corresponding view for this component
-        const menu=this.state.dishes.map((dish)=>{
+        const menu=this.props.dishes.map((dish)=>{
             //map operator is going to iterate over all the dishes items (every dish)
             return(
                 //this return will return layout for every dish
@@ -64,18 +47,18 @@ class Menu extends Component{
                /*whenever we construct an item,every item required a key attribute,
                These key helps react rendering the items on the screen */
                //Learn media class of reactstrap from react documentation
-                <div key={dish.id} className="col-12 mt-5">
-                    <Media tag="li">
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                        </Media>
-                        <Media body className="ml-5">
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={()=>this.onDishSelect(dish)}>
+                       
+                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    
+                        <CardImgOverlay>
 
-                            <Media heading >{dish.name}</Media>
-                            <p>{dish.description}</p>
+                            <CardTitle >{dish.name}</CardTitle>
+                            
 
-                        </Media>
-                    </Media>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             )
         });
@@ -85,10 +68,10 @@ class Menu extends Component{
             //This {menu} under media class is going a display a list of item for the restaurant
            <div className="container">
                <div className="row">
-                    <Media list>
-                        
-                            {menu}
-                    </Media>
+                     {menu}
+               </div>
+               <div className="row">
+                   {this.renderDish(this.state.selectedDish)}
                </div>
            </div>
         );
