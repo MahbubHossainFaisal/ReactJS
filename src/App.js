c8be263a252610a31ec005e7a71f45425c337886
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-09-07 02:55:16 +0600

    React Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from "react";
 import Main from "./components/MainComponent.js";
 import "./App.css";
+import {BrowserRouter} from "react-router-dom";
 
 
 class App extends Component {
 
     
-
     render() {
-        return (<
-            div>
-           <Main />;
-        </div>
+        return (
+          <BrowserRouter>
+            <div>
+              <Main />;
+            </div>
+          </BrowserRouter>
         );
     }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,6 +6,8 @@ import DishDetail from './DishdetailComponent.js';
 import Header from './HeaderComponent.js';
 import Footer from './FooterComponent.js';
 import { DISHES } from "../shared/dishes";
+import Home from './HomeComponent.js'
+import {Switch , Route ,Redirect } from "react-router-dom";
 
 class Main extends Component {
 
@@ -13,20 +15,26 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish:null
+           
         };
     }
-    onDishSelect(dishId) {
-        this.setState({ selectedDish: dishId });
-    }
+    
 
     render() {
+        const HomePage = () => {
+            return(
+                <Home />
+            );
+        }
         return (
         <div>
             <Header />
-            <Menu dishes={this.state.dishes} 
-                onClick={(dishId) => this.onDishSelect(dishId)} />
-            <DishDetail dish={this.state.dishes.filter((dish)=> dish.id === this.state.selectedDish)[0]} />
+            <Switch>
+                <Route path="/home" component={HomePage} />
+                <Route exact path = "/menu" component={()=> <Menu dishes={this.state.dishes} />} />
+                <Redirect to="/home" />
+            </Switch>
+           
             <Footer />
         </div>
         );
