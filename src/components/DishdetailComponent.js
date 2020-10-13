12befc4a84ba88a319f5bfc23a652eb1accd54b9
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-14 02:10:51 +0600

    Adding Breadcrumb,Breadcrumbitem in DishdetailComponent

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,7 +5,11 @@ import {
   CardTitle,
   CardBody,
   CardText,
+  BreadcrumbItem,
+  Breadcrumb,
 } from "reactstrap";
+import {Link} from 'react-router-dom';
+
 
 function RenderDish({dish}){
   if (dish != null) {
@@ -53,6 +57,17 @@ function RenderDish({dish}){
     const DishDetail = (props)=>{
       return (
         <div className="container">
+           <div className='row'>
+              <Breadcrumb>
+                
+                 <BreadcrumbItem> <Link to='/menu'>Menu</Link> </BreadcrumbItem>
+                 <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+              </Breadcrumb>
+              <div className='col-12'>
+                <h3> {props.dish.name} </h3>
+                <hr />
+              </div>
+            </div>
           <div className="row">
             <div className="col-12 col-sm-5 m-1">
               <RenderDish dish={props.dish} />
@@ -60,7 +75,7 @@ function RenderDish({dish}){
             <div className="col-12 col-sm-5 m-1">
               <ul>
                 <h4>Comments</h4>
-                <RenderDishComments  dish={props.dish}/>
+                <RenderDishComments  dish={props.comments}/>
               </ul>
             </div>
           </div>
