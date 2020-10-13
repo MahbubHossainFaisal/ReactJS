4a8d3807af925beed5c32764d53733b062ef8a3e
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-14 02:48:01 +0600

    Single page applications part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -14,73 +14,76 @@ import {Link} from 'react-router-dom';
 function RenderDish({dish}){
   if (dish != null) {
     return (
-      <Card>
+     <div className="col-12 col-md-5 m-1">
+        <Card>
         <CardImg src={dish.image} alt="dishImage" />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
         </CardBody>
       </Card>
+     </div>
     );
   } else {
     return <div></div>;
   }
 }
  
-  const RenderDishComments = ({dish})=>{
-    if (dish != null) {
-      const comments = dish.comments.map((value) => {
-        return (
-          <li key={value.id}>
-            <div>{value.comment}</div>
-            <div>
-              {"-- "}
-              {value.author} {" "}
-              {value.date}{" "}
-              ,{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(value.date)))}
-            </div>
-          </li>
-        );
-      });
-      return (
-        <div className="list-unstyled" list>
-          {comments}
-        </div>
-      );
-    } else {
-      return <div></div>;
-    }
+  const RenderDishComments = ({comments})=>{
+   if(comments != null){
+     return(
+      <div className="col-12 col-md-5 m-1">
+        <h4>Comments</h4>
+        <ul className="list-unstyled">
+          {
+            comments.map((comment)=>{
+              return(
+                <li key={comment.id}>
+                  <p>{comment.comment}</p>
+                  <p>-- {comment.author},{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))} </p>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+     );
+   }
+   else {
+     return(
+       <div> </div>
+     );
+   }
   }
 
 
   
     const DishDetail = (props)=>{
-      return (
-        <div className="container">
-           <div className='row'>
+      if(props.dish != null){
+        return(
+          <div className="container">
+             <div className='row'>
               <Breadcrumb>
-                
-                 <BreadcrumbItem> <Link to='/menu'>Menu</Link> </BreadcrumbItem>
-                 <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                <BreadcrumbItem> <Link to='/home'>Home</Link> </BreadcrumbItem>
+                <BreadcrumbItem active> {props.dish.name} </BreadcrumbItem>
               </Breadcrumb>
               <div className='col-12'>
                 <h3> {props.dish.name} </h3>
                 <hr />
               </div>
             </div>
-          <div className="row">
-            <div className="col-12 col-sm-5 m-1">
+            <div className="row">
               <RenderDish dish={props.dish} />
-            </div>
-            <div className="col-12 col-sm-5 m-1">
-              <ul>
-                <h4>Comments</h4>
-                <RenderDishComments  dish={props.comments}/>
-              </ul>
+              <RenderDishComments comments={props.comments} />
             </div>
           </div>
-        </div>
-      );
+        );
+      }
+      else{
+        return(
+          <div></div>
+        );
+      }
     }
     
 
