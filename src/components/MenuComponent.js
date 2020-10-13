3f33fe1cf14943172195c7e93863fd87aa15e46d
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-14 01:59:00 +0600

    adding one more route path and adding DishWithId function so that the new route path can work

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -33,6 +33,10 @@ import {Link} from 'react-router-dom';
                 <BreadcrumbItem> <Link to='/home'>Home</Link> </BreadcrumbItem>
                 <BreadcrumbItem active>Menu</BreadcrumbItem>
               </Breadcrumb>
+              <div className='col-12'>
+                <h3>Menu</h3>
+                <hr />
+              </div>
             </div>
             <div className='row'>
               {menu}
