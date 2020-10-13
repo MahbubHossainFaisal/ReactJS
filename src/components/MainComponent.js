0aff7efa0cf4515fe0af8f03269c507c4eee7e16
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-14 02:00:32 +0600

    Route path and DishWithId function was created in MainComponent

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -55,6 +55,7 @@ class Main extends Component {
                 <Route path="/home" component={HomePage} />
                 <Route exact path = "/menu" component={()=> <Menu dishes={this.state.dishes} />} />
                 <Route path = "/menu/:dishId" component={DishWithId} />
+                
                 <Route exact path="/contactus" component={Contact} />
                 <Redirect to="/home" />
             </Switch>
