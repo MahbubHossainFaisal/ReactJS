efdb5d987915cfd9cfc34947234790c679a0ed16
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-10-24 17:19:45 +0600

    Adding handleInputChange method in ContactComponent

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -21,6 +21,13 @@ class Contact extends Component {
 
     handleInputChange=(event) => {
 
+        const target = event.target;
+        const value = target.type === "checkbox" ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name] : value,
+        })
     }
 
     handleSubmit = (event) => {
