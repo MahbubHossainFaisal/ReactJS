25eb9a3893d259b10c214e171b453b8973686db1
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-11-07 18:08:44 +0600

    Updating dishes.js file under redux folder with new cases

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,8 +1,22 @@
-import { DISHES } from '../shared/dishes';
+import * as ActionTypes from './ActionTypes';
 
 
-export const Dishes = (state = DISHES,action) => {
+export const Dishes = (state = {
+    isLoading: true,
+    errmess: null,
+    dishes: []
+},action) => {
     switch(action.type){
+        case ActionTypes.ADD_DISHES:
+
+
+        case ActionTypes.DISHES_LOADING:
+            return {...state, isLoading:false ,errmess:null,dishes:action.payload}
+
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading:false ,errmess:action.payload,dishes:[]}
+
+
         default:
             return state;
     }
