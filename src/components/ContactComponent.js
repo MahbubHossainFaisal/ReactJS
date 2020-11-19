966f1018a594e8e0f211d621693d05e166e07873
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2020-11-20 00:17:11 +0600

    Assignment 4

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -22,7 +22,7 @@ class Contact extends Component {
    
 
     handleSubmit(values){
-        
+        alert('Current State is: ' + JSON.stringify(values));
         this.props.resetFeedbackForm();
         this.props.postFeedback(values);
     }
